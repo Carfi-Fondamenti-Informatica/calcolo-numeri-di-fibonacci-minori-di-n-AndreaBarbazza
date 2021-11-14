4be736d7edade431f364f84640b02eb80deae2eb
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-11-14 10:33:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main() {
+   
+   int n=0;
+   int a=0, b=1, c=a+b;
+   cin >> n; 
+   
+   if (n==0){
+      return 0;
+   }else{
+      cout << b << endl;
+   }
+   
+   for (int i=1; i<=n; i++){
+      
+      c=a+b;
+      a=b;
+      b=c;
+      
+      if (c>n){
+         return 0;
+      }else{
+         cout << c << endl;
+      }
+   }
+   
    return 0;
 }
